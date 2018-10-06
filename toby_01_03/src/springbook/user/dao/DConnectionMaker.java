80f3f257eaf5b42c8e8ae050d324b22ead637e16
jung <jungha0902@naver.com>
2018-10-06 19:42:27 +0900

    1.3 DAO의 확장 - 인터페이스의 도입

--- toby_01_03/src/springbook/user/dao/DConnectionMaker.java
+++ toby_01_03/src/springbook/user/dao/DConnectionMaker.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//독립시킨 DB 연결 기능인 SimpleConnectionMaker
-public class SimpleConnectionMaker { // 더 이상 상속을 이용한 확장 방식을 사용할 필요가 없으니 추상 클래스도 만들 필요 없음.
-	public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+public class DConnectionMaker implements ConnectionMaker{
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		// D 사의 독자적인 방법으로 Connection을 생성하는 코드
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_spring?characterEncoding=UTF-8","root","258080");
 		return c;
 	}
+	
 }
--- toby_01_03/src/springbook/user/dao/UserDao.java
+++ toby_01_03/src/springbook/user/dao/UserDao.java
@@ -10,17 +10,19 @@ import springbook.user.domain.User;
 
 // JDBC를 이용한 등록과 조회 기능이 있는 UserDao 클래스
 public class UserDao {
-	private SimpleConnectionMaker simpleConnectionMaker;
+	private ConnectionMaker connectionMaker; //인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다. 
 	
 	public UserDao() {
-		// 상태를 관리하는 것도 아니니 한 번만 만들어 인스턴스 변수에 저장해두고 메소드에서 사용하게 한다. 
-		simpleConnectionMaker = new SimpleConnectionMaker();
+		//상태를 관리하는 것도 아니니 한 번만 만들어 인스턴스 변수에 저장해두고 메소드에서 사용하게 한다. 
+		connectionMaker = new DConnectionMaker(); //but.. DConnectionMaker클래스의 생성자를 호출해서 오브젝트를 생성하는 코드가 여전히 UserDao에 남아있다!!!  
 	}
 	
 	// 새로운 사용자를 add.
 	public void add(User user) throws ClassNotFoundException, SQLException{
 		
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		//인터페이스에 정의된 메소드를 사용하므로
+		//클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다. 
+		Connection c = connectionMaker.makeConnection(); 
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -36,7 +38,7 @@ public class UserDao {
 	//아이디를 가지고 사용자 정보를 get.
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		
-		Connection c = simpleConnectionMaker.makeNewConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
