fdc0b0c239980fdd3441088f1518bad5a9a41c74
jung <jungha0902@naver.com>
2018-10-17 23:51:19 +0900

    1.7 의존관계 주입(DI) - 의존관계 주입의 응용 - 부가기능 추가(DB연결 횟수 카운팅 기능이 있는 클래스 추가)

--- toby_01_07/src/main/java/springbook/user/dao/CountingDaoFactory.java
+++ toby_01_07/src/main/java/springbook/user/dao/CountingDaoFactory.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
-public class DaoFactory {
+public class CountingDaoFactory {
 	@Bean 
 	public UserDao userDao(){
-		return new UserDao(connectionMaker());
+		return new UserDao(connectionMaker()); //모든 DAO는 여전히 connectionMaker()에서 만들어지는 오브젝트를 DI받는다.
 	}
-
 	@Bean
 	public ConnectionMaker connectionMaker(){
-		return new DConnectionMaker(); 
+		return new CountingConnectionMaker(realConnectionMaker()); 
+	}
+	@Bean 
+	public ConnectionMaker realConnectionMaker(){
+		return new DConnectionMaker();
 	}
 }
--- toby_01_07/src/main/java/springbook/user/dao/UserDao.java
+++ toby_01_07/src/main/java/springbook/user/dao/UserDao.java
@@ -11,29 +11,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import springbook.user.domain.User;
 
 public class UserDao {
-	/* 
-	 * 의존관계 '검색'은 런타임 시 의존관계를 맺을 오브젝트를 결정하는 것과 오브젝트의 생성 작업은 외부 컨테이너에게 IoC로 맡기지만,
-	 * 이를 가져올 때는 메소드나 생성자를 통한 주입 대신 스스로 컨테이너에게 요청하는 방법을 사용.
-	 */
+
 	private ConnectionMaker connectionMaker; 
 
-	//old (의존관계 주입을 위한 코드)
-//	public UserDao(ConnectionMaker connectionMaker) {
-//		this.connectionMaker = connectionMaker; 
-//	}
-	
-	//new 
-	//의존관계 검색을 이용하는 UserDao 생성자
-	public UserDao() {
-		//DaoFactory를 이용하는 생성자
-		//(ConnectionMaker 타입 오브젝트를 외부로부터의 주입이 아니라 스스로 IoC컨테이너인 DaoFactory에게 요청)
-//		DaoFactory daoFactory = new DaoFactory();
-//		this.connectionMaker = daoFactory.connectionMaker();
-		
-		//=> 이런 작업을 일반화한 스프링의 애플리케이션 컨텍스트라면 미리 정해놓은 이름을 전달해서 그 이름에 해당하는 오브젝트를 찾는다.(검색)
-		//또한 그 대상이 런타임 의존관계를 가질 오브젝트이므로 '의존관계 검색'이라 부름.
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		this.connectionMaker = context.getBean("connectionMaker",ConnectionMaker.class);
+	public UserDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker; 
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException{
