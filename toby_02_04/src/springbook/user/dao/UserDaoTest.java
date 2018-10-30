00cbc3b45e737217f80e443738963fce20c3904e
DESKTOP-VPABIR5\JUNG <jungha0902@gmail.com>
2018-10-30 11:36:50 +0900

    2.4.2 DI와 테스트 - 테스트를 위한 별도의 DI 설정 (테스트에서 사용될 DataSource클래스가 빈으로 정의된 테스트 전용 설정파일을 따로 생성)

--- toby_02_04/src/springbook/user/dao/UserDaoTest.java
+++ toby_02_04/src/springbook/user/dao/UserDaoTest.java
@@ -21,11 +21,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)//스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정.
-@ContextConfiguration(locations="/applicationContext.xml")//테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정. 
-@DirtiesContext // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컨텍스트 프레임워크에게 알려줌. 
- 				// - 테스트 컨텍스트는 이 애노테이션이 붙은 테스트 클래스에는 애플리케이션 컨텍스트 공유를 허용하지 않는다. 
-				// - 메소드 레벨에도 @DirtiesContext 사용가능. 하나의 메소드드에서만 컨텍스트 상태를 변경한다면 해당 메소드에 @DirtiesContext를 붙여줌. 
-				//   해당 메소드의 실행이 끝나면 이후 진행되는 테스트를 위해 변경된 애플리케이션 컨텍스트는 폐기 된다. 
+@ContextConfiguration(locations="/test-applicationContext.xml")//테스트용 
 public class UserDaoTest {
 	@Autowired
 	private UserDao dao; //setUp()메소드에서 만드는 오브젝트를 테스트 메소드에서 사용할 수 있도록 인스턴스 변수로 선언.
@@ -36,10 +32,6 @@ public class UserDaoTest {
 	
 	@Before //JUnit 제공 어노테이션. @Test 메소드가 실행되기 전에 먼저 실행돼야하는 메소드를 정의.
 	public void setUp(){
-		
-		//테스트에서 UserDao가 사용할 DateSource오브젝트를 직접 생성.
-		DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost:3306/testdb?characterEncoding=UTF-8","root","1234",true);
-		dao.setDataSource(dataSource); //코드에 의한 수동 DI
 		
 		this.user1 = new User("user1","유저일","springno1");
 		this.user2 = new User("user2","유저이","springno2");
