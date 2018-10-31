6ab1bd20606452e9067e589a9e5cf6990f4b2477
DESKTOP-VPABIR5\JUNG <jungha0902@gmail.com>
2018-10-31 17:40:56 +0900

    2.5.2 학습 테스트 예제 - 개선한 JUnit 테스트 오브젝트 생성에 대한 학습 테스트

--- toby_02_05/src/springbook/learningtest/junit/JUnitTest.java
+++ toby_02_05/src/springbook/learningtest/junit/JUnitTest.java
@@ -1,33 +1,37 @@
 package springbook.learningtest.junit;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
+/* 테스트마다 현재 테스트 오브젝트가 컬렉션에 이미 등록되어 있는지 확인하고, 없으면 자기 자신을 추가. 
+   ->테스트가 어떤 순서로 실행되는지에 상관없이 오브젝트 중복 여부를 확인 가능. */
 public class JUnitTest {
-	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 	
 	@Test
 	public void test1(){
 		//is()는 equals() 비교를해서 같으면 성공이지만, is(not())은 반대로 같지 않아야 성공.
-		//sameInstance()는 실제로 같은 오브젝트인지를 비교.
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		//hasItem()는 실제로 같은 오브젝트인지를 비교.
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test
 	public void test2(){
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 
 	@Test
 	public void test3(){
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 }
