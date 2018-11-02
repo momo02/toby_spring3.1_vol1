5ca1e0c7780085dd05c0802c4e4998d7ed6f5f0c
DESKTOP-VPABIR5\JUNG <jungha0902@gmail.com>
2018-11-02 15:41:49 +0900

    3.3.2 전략과 클라이언트의 동거 - 익명 내부 클래스

--- toby_03_03/src/springbook/user/dao/UserDao.java
+++ toby_03_03/src/springbook/user/dao/UserDao.java
@@ -22,29 +22,47 @@ public class UserDao {
 	// *내부 클래스에서 외부의 변수를 사용할 때는 외부 변수는 반드시 final로 선언해줘야 함.
 	// (user파라미터는 메소드 내부에서 변경될 일이 없으므로 final로 선언해도 무방)
 	public void add(final User user) throws ClassNotFoundException, SQLException{
-		// add 메소드 내의 로컬 클래스로 이전한 AddStatement 
-		// (UserDao에서만 사용되고, UserDao의 메소드 로직에 강하게 결합되어 있음) 
-		class AddStatement implements StatementStrategy{
-//old
-//			User user;
-//			
-//			//User정보를 생성자로부터 제공받도록 함.
-//			public AddStatement(User user){
-//				this.user = user;
+
+// old		
+//		// add 메소드 내의 로컬 클래스로 이전한 AddStatement 
+//		// (UserDao에서만 사용되고, UserDao의 메소드 로직에 강하게 결합되어 있음) 
+//		class AddStatement implements StatementStrategy{
+//			public PreparedStatement makeStatement(Connection c) throws SQLException {
+//				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+//				ps.setString(1, user.getId());
+//				ps.setString(2, user.getName());
+//				ps.setString(3, user.getPassword());
+//				return ps;
 //			}
-			
+//		}
+
+//new -> 익명 내부 클래스로 전환
+		/* 익명 내부 클래스(anonymous inner class)는 이름을 갖지 않는 클래스. 클래스 선언과 오브젝트 생성이 결합된 형태. 
+		 * 상속할 클래스나 구현할 인터페이스를 생성자 대신 사용해 아래와 같은 형태로 만들어 사용.
+		 * ex) new 인터페이스이름() { 클래스 본문 };
+		 * 클래스를 재사용할 필요가 없고, 구현한 인터페이스 타입으로만 사용할 경우에 유용.
+		 */
+//		StatementStrategy st = new StatementStrategy() { 
+//			public PreparedStatement makeStatement(Connection c) throws SQLException {
+//				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+//				ps.setString(1, user.getId());                                                                   
+//				ps.setString(2, user.getName());                                                                 
+//				ps.setString(3, user.getPassword());                                                             
+//				return ps;                                                                                       
+//			}
+//		};
+		
+		//위의 만들어진 익명 내부 클래스의 오브젝트는 딱 한번만 사용할 테니, 굳이 변수에 담아두지 말고
+		//jdbcContextWithStatementStrategy()메소드의 파라미터에서 바로 생성하는 편이 낫다.
+		jdbcContextWithStatementStrategy(new StatementStrategy() { 
 			public PreparedStatement makeStatement(Connection c) throws SQLException {
 				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				return ps;
+				ps.setString(1, user.getId());                                                                   
+				ps.setString(2, user.getName());                                                                 
+				ps.setString(3, user.getPassword());                                                             
+				return ps;                                                                                       
 			}
-		}
-		
-//		StatementStrategy st = new AddStatement(user);
-		StatementStrategy st = new AddStatement(); //생성자 파라미터로 user를 전달하지 않아도 됨
-		jdbcContextWithStatementStrategy(st);
+		});
 	}
 	
 	// id에 해당하는 데이터가 없으면 EmptyResultDataAccessException 에러를 던지도록 수정한 get메서드
@@ -75,11 +93,20 @@ public class UserDao {
 	
 	//USERS 테이블의 모든 레코드를 삭제
 	public void deleteAll() throws SQLException {
-		// 클라이언트 책임을 갖도록 재구성한 deleteAll() 메소드
-		// -> deleteAll()은 전략 오브젝트(DeleteAllStatement)를 만들고 컨텍스트를 호출하는 책임을 지고 있다.
-		// 클라이언트가 컨텍스트가 사용할 전략을 정해서 전달한다는 면에서 DI구조라고 이해할 수도 있다.
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+//old
+//		// 클라이언트 책임을 갖도록 재구성한 deleteAll() 메소드
+//		// -> deleteAll()은 전략 오브젝트(DeleteAllStatement)를 만들고 컨텍스트를 호출하는 책임을 지고 있다.
+//		// 클라이언트가 컨텍스트가 사용할 전략을 정해서 전달한다는 면에서 DI구조라고 이해할 수도 있다.
+//		StatementStrategy st = new DeleteAllStatement();
+//		jdbcContextWithStatementStrategy(st);
+		
+//new -> 익명 내부 클래스로 전환
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makeStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("delete from users");
+				return ps;
+			}
+		});
 	}
 	
 	//USERS 테이블의 레코드 개수를 돌려줌
