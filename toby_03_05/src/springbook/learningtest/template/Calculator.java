ad15114e1d12fc08c125b6d3755345c10efb7a1b
DESKTOP-VPABIR5\JUNG <jungha0902@gmail.com>
2018-11-05 10:08:31 +0900

    3.5.3 템플릿/콜백의 응용 - calcSum()메소드에 try/catch/finally를 적용

--- toby_03_05/src/springbook/learningtest/template/Calculator.java
+++ toby_03_05/src/springbook/learningtest/template/Calculator.java
@@ -7,14 +7,25 @@ import java.io.IOException;
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException{
 		//한 줄씩 읽기 편하게 BufferedReader로 파일을 가져온다.
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		Integer sum = 0;
-		String line = null;
-		//마지막 라인까지 한 줄씩 읽어가면서 숫자를 더한다.
-		while((line = br.readLine()) != null){
-			sum += Integer.valueOf(line);
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new FileReader(filepath));
+			Integer sum = 0;
+			String line = null;
+			//마지막 라인까지 한 줄씩 읽어가면서 숫자를 더한다.
+			while((line = br.readLine()) != null){
+				sum += Integer.valueOf(line);
+			}
+			return sum;
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+			throw e;
+		}finally{
+			//BufferedReader오브젝트가 생성되기 전에 예외가 발생할 수도 있으므로 반드시 null체크 먼저!  
+			if(br != null){  
+				try{ br.close(); }
+				catch(IOException e){ System.out.println(e.getMessage()); }
+			}
 		}
-		br.close(); //한 번 연 파일은 반드시 닫아줌.
-		return sum;
 	}
 }
