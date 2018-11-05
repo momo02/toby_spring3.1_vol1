d4fa512cf4335578f27aa68c76ea4aa5bfc27add
DESKTOP-VPABIR5\JUNG <jungha0902@gmail.com>
2018-11-05 11:44:17 +0900

    3.5.3 템플릿/콜백의 응용 - 템플릿/콜백의 재설계

--- toby_03_05/src/springbook/learningtest/template/Calculator.java
+++ toby_03_05/src/springbook/learningtest/template/Calculator.java
@@ -6,17 +6,70 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException{
-//old
+//old		
+//		// 템플릿/콜백을 적용한 calcSum() 메소드
+//		BufferedReaderCallback sumCallback = new BufferedReaderCallback(){
+//			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+//				Integer sum = 0;             
+//				String line = null;          
+//				while((line = br.readLine()) != null){     
+//					sum += Integer.valueOf(line);          
+//				}                                          
+//				return sum;                                
+//			}
+//		};
+//		//템플릿이 리턴하는 값을 최종 결과로 사용한다.
+//		return fileReadTemplate(filepath, sumCallback);
+		
+		//new 
+		LineCallback sumCallback = new LineCallback(){
+			public Integer doSomethingWithLine(String line, Integer value) {
+				//value += Integer.valueOf(line);
+				//return value;
+				return value + Integer.valueOf(line); //루프를 돌면서 라인 한줄을 읽을때마다 수행하는 동작.
+			}
+		};
+		//템플릿이 리턴하는 값을 최종 결과로 사용한다.
+		return lineReadTemplate(filepath, sumCallback, 0);
+	}
+	
+	
+	// 곱을 계산하는 콜백을 가진 calMutiply() 메소드 
+	public Integer calMutiply(String filepath) throws IOException{
+		
+//old		
+//		BufferedReaderCallback mutiplyCallback = new BufferedReaderCallback(){
+//			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+//				Integer mutiply = 1;             
+//				String line = null;          
+//				while((line = br.readLine()) != null){     
+//					mutiply *= Integer.valueOf(line);          
+//				}                                          
+//				return mutiply;                                
+//			}
+//		};
+//		//템플릿이 리턴하는 값을 최종 결과로 사용한다.
+//		return fileReadTemplate(filepath, mutiplyCallback);
+		
+		//new
+		LineCallback mutiplyCallback = new LineCallback(){
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value * Integer.valueOf(line); 
+			}
+		};
+		return lineReadTemplate(filepath, mutiplyCallback, 1);
+	}
+	
+//old	
+//	// BufferedReaderCallback을 사용하는 fileRead 템플릿 메소드
+//	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
 //		BufferedReader br = null;
 //		try{
 //			br = new BufferedReader(new FileReader(filepath));
-//			Integer sum = 0;
-//			String line = null;
-//			//마지막 라인까지 한 줄씩 읽어가면서 숫자를 더한다.
-//			while((line = br.readLine()) != null){
-//				sum += Integer.valueOf(line);
-//			}
-//			return sum;
+//			//콜백 오브젝트 호출. 템플릿에서 만든 컨텍스트 정보인 BufferedReader를 전달해주고 콜백의 작업 결과를 받아둔다.
+//			int ret = callback.doSomethingWithReader(br);
+//			return ret;
+//			
 //		}catch(IOException e){
 //			System.out.println(e.getMessage());
 //			throw e;
@@ -27,57 +80,32 @@ public class Calculator {
 //				catch(IOException e){ System.out.println(e.getMessage()); }
 //			}
 //		}
-		
-		//new -> 템플릿/콜백을 적용한 calcSum() 메소드
-		BufferedReaderCallback sumCallback = new BufferedReaderCallback(){
-			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-				Integer sum = 0;             
-				String line = null;          
-				while((line = br.readLine()) != null){     
-					sum += Integer.valueOf(line);          
-				}                                          
-				return sum;                                
-			}
-		};
-		//템플릿이 리턴하는 값을 최종 결과로 사용한다.
-		return fileReadTemplate(filepath, sumCallback);
-		
-	}
-	
-	//new -> 곱을 계산하는 콜백을 가진 calMutiply() 메소드 
-	public Integer calMutiply(String filepath) throws IOException{
-		BufferedReaderCallback mutiplyCallback = new BufferedReaderCallback(){
-			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-				Integer mutiply = 1;             
-				String line = null;          
-				while((line = br.readLine()) != null){     
-					mutiply *= Integer.valueOf(line);          
-				}                                          
-				return mutiply;                                
-			}
-		};
-		//템플릿이 리턴하는 값을 최종 결과로 사용한다.
-		return fileReadTemplate(filepath, mutiplyCallback);
-	}
+//	}
 	
 	
-	//new -> BufferedReaderCallback을 사용하는 템플릿 메소드
-	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
+	//new -> LineCallback을 사용하는 템플릿 메소드
+	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal/*계산 결과를 저장할 변수의 초기값*/) 
+			throws IOException{
 		BufferedReader br = null;
 		try{
 			br = new BufferedReader(new FileReader(filepath));
-//old 			
-//			Integer sum = 0;
-//			String line = null;
-//			//마지막 라인까지 한 줄씩 읽어가면서 숫자를 더한다.
-//			while((line = br.readLine()) != null){
-//				sum += Integer.valueOf(line);
-//			}
-//			return sum;
 			
-			//new -> 콜백 오브젝트 호출. 템플릿에서 만든 컨텍스트 정보인 BufferedReader를 전달해주고 콜백의 작업 결과를 받아둔다.
-			int ret = callback.doSomethingWithReader(br);
-			return ret;
+//			Integer mutiply = 1;             
+//			String line = null;          
+//			while((line = br.readLine()) != null){     
+//				mutiply *= Integer.valueOf(line);          
+//			}                                          
+//			return mutiply;        
+			
+			//new 
+			int res = initVal;
+			String line = null; 
+			while((line = br.readLine()) != null){ //파일의 각 라인을 루프를 돌면서 가져오는 것도 템플릿이 담당.
+				res = callback.doSomethingWithLine(line, res);
+				//res -> 콜백이 계산한 값을 저장해뒀다가 다음 라인 계산에 다시 사용한다.
+				//각 라인의 내용을 가지고 계산하는 작업만 콜백에게 맡긴다.
+			}
+			return res;
 			
 		}catch(IOException e){
 			System.out.println(e.getMessage());
@@ -90,5 +118,5 @@ public class Calculator {
 			}
 		}
 	}
-	
+
 }
