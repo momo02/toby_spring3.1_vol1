e9887699f03dfa78d6e7fbdd4393a26a85954123
momo02 <jungha0902@gmail.com>
2019-05-08 16:43:57 +0900

    7.2.3 빈의 초기화 작업 - SQL을 담은 XML파일의 위치와 이름을 DI로 설정하도록 변경 - 빈 후처리기 등록 및 @PostConstruct 어노테이션을 초기화 작업을 수행할 메소드에 부여

--- toby_07_02/src/springbook/user/sqlservice/XmlSqlService.java
+++ toby_07_02/src/springbook/user/sqlservice/XmlSqlService.java
@@ -3,8 +3,8 @@ package springbook.user.sqlservice;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.zip.InflaterInputStream;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -19,14 +19,38 @@ public class XmlSqlService implements SqlService {
 	
 	private Map<String, String> sqlMap = new HashMap<String, String>(); 
 	
-	//스프링이 오브젝트를 만드는 시점에서 SQL을 읽어오도록 생성자를 이용.
-	public XmlSqlService() {
+	//new :: sql을 담은 XML파일의 위치와 이름이 코드에 고정되지않고, 
+	//외부에서 DI로 설정해줄 수 있도록 변경
+	private String sqlmapFile;
+
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+
+	@Override
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		String sql = sqlMap.get(key);
+		if(sql == null) {
+			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
+		}else 
+			return sql;
+	}
+	
+	//new :: 생성자 대신 사용할 초가화 메소드
+	//	=> 생성자에서 예외가 발생할 수도 있는 복잡한 초기화 작업을 다루는것은 좋지 않다.
+	//     생성자에서 발생하는 예외는 다루기 힘들고, 상속하기 불편하며, 보안에도 문제가 생길 수 있음.
+	
+	// @PostConstruct -> 빈의 초기화 메소드로 지정
+	// 스프링은 빈의 오브젝트를 생성하고 DI작업을 마친 뒤에 @PostConstruct가 붙은 메소드를 자동으로 실행해준다. 
+	// 생성자와는 달리 프로퍼티까지 모두 준비된 후에 실행된다는 면에서 매우 유용!
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {
 			//JAXB API를 이용해 XML문서를 오브젝트 트리로 읽어온다.
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml"); //UserDao와 같은 클래스 패스의 sqlmap.xml파일을 변환.
+			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile); //UserDao와 같은 클래스 패스의 sqlmap.xml파일을 변환.
 			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 			
 			//읽어온 SQL을 맵으로 저장해둔다.
@@ -37,14 +61,5 @@ public class XmlSqlService implements SqlService {
 			//JAXBException은 복구 불가능한 예외. 불필요한 throws를 피하도록 런타임 예외로 포장해 던짐.
 			throw new RuntimeException(e);
 		}
-	}
-
-	@Override
-	public String getSql(String key) throws SqlRetrievalFailureException {
-		String sql = sqlMap.get(key);
-		if(sql == null) {
-			throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
-		}else 
-			return sql;
 	}
 }
